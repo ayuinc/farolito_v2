cde1f977cbc6adac03a0d8ab8d8e00c9c6874333
Mercedes Zubieta <mercedes.zubieta@gmail.com>
2015-03-23 15:20:17 -0500

    background y cambio de logo de Sobre nosotros

---
+++ js/coffeeCompile.js
@@ -0,0 +1,35 @@
+(function() {
+  'use-strict';
+  $(document).ready(function() {
+    var scrollToController;
+    scrollToController = new ScrollMagic();
+    scrollToController.scrollTo(function(newpos) {
+      TweenMax.to(window, 0.5, {
+        scrollTo: {
+          y: newpos
+        }
+      });
+    });
+    $(document).on('click', 'a[href^=#]', function(e) {
+      var id;
+      id = $(this).attr('href');
+      if ($(id).length > 0 && !id.match(/nav/g) && !id.match(/carousel/g)) {
+        e.preventDefault();
+        scrollToController.scrollTo(id);
+      }
+    });
+    // Header estático
+    var headerController = new ScrollMagic.Controller();
+    var headerScene = new ScrollMagic.Scene({
+      triggerHook: 'onEnter',
+      triggerElement: '.header-trigger'
+      // duration: $(item).height()
+      })
+      .setClassToggle('.site-wrapper', 'header-fixed')
+      .addTo(headerController);
+  });
+
+}).call(this);
+
+
+
